492181eaae96756b6da8e63b585d679a875f81b0
almir-handabaka <73104081+almir-handabaka@users.noreply.github.com>
2022-06-15 22:51:21 +0200

    network

--- webApi2/app/src/main/java/com/example/webapi/PocetnaFragment.kt
+++ webApi2/app/src/main/java/com/example/webapi/PocetnaFragment.kt
@@ -4,13 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.*
-import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.webapi.databinding.FragmentPocetnaBinding
+import com.example.webapi.network.OverviewViewModel
+
 
 class PocetnaFragment : Fragment() {
+    private val viewModel: OverviewViewModel by lazy {
+        ViewModelProvider(this).get(OverviewViewModel::class.java)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +36,14 @@ class PocetnaFragment : Fragment() {
 
         binding.daljeButton.setOnClickListener { view : View ->
             Log.i("problem", "usao u dugme za dalje")
+
+            viewModel.properties.observe(viewLifecycleOwner, Observer{kvalute->
+                for(i in 0..kvalute.size){
+                    Log.i("problem", kvalute[i].toString())
+                }
+            })
+            Log.i("problem", viewModel.properties.toString())
+
             view.findNavController().navigate(R.id.action_pocetnaFragment_to_listaFragment)
 
         }
